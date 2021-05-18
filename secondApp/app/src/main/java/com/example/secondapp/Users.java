7c31497d6ae2ac3f50284f57c5826a32eca34c1d
AK <akovtuns@yandex.ru>
2021-05-18 19:12:04 +0300

    18_05

---
+++ secondApp/app/src/main/java/com/example/secondapp/Users.java
@@ -0,0 +1,18 @@
+package com.example.secondapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Users {
+    private List<String> userList;
+    public List<String> getUserList(){
+        userList = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            this.userList.add("Человек_"+i);
+        }
+        return this.userList;
+    }
+
+
+}
